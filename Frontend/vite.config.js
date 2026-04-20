fd4f513fe700dfa5080c4200f98d594ebcf7ac83
Mushab Rahman <mushabrahman02@gmail.com>
2026-04-20 15:29:14 +0530

    bug fixed

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   server:{
     proxy:{
-     '/api': "https://webtinder.in:8000"
+     '/api': "https://webtinder.in"
     }
   },
   plugins: [react(),
